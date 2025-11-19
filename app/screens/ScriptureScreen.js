64740d529d27301791fc64ae1cdbca6d0644061b
ChristTalks <productyz@gmail.com>
2025-11-19 11:54:00 -0600

    Create ScriptureScreen.js

---
+++ app/screens/ScriptureScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ScriptureScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'white' }}>Scripture Module Coming Soon</Text>
+    </View>
+  );
+}
