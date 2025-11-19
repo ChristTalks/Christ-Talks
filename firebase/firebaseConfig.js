f3d772d18fa028d49ea7f6c5879e0738038d7f98
ChristTalks <productyz@gmail.com>
2025-11-19 11:48:18 -0600

    Create firebaseConfig.js

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebase/firebaseConfig.js
+// Replace the contents with your Firebase config and initialization when ready.
+// Example:
+// import { initializeApp } from "firebase/app";
+// const firebaseConfig = { apiKey: "...", authDomain: "...", projectId: "...", ... };
+// const app = initializeApp(firebaseConfig);
+// export default app;
+
+export default null;
