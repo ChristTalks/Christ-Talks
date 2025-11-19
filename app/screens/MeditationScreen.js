c9bfd972a419e98d7a43b5609d5b6b80c3387fa4
ChristTalks <productyz@gmail.com>
2025-11-19 11:53:26 -0600

    Create MeditationScreen.js

---
+++ app/screens/MeditationScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MeditationScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'white' }}>Meditation Module Coming Soon</Text>
+    </View>
+  );
+}
